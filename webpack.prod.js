132ba87d253f1b9376a219b029c2d35cf0cf0d70
Miguel Lopez <chino6989@gmail.com>
2020-08-25 13:27:23 -0400

    UPDATED template file name to index.html on line 21

--- webpack.prod.js
+++ webpack.prod.js
@@ -18,7 +18,7 @@ module.exports = merge(common, {
       new OptimizeCssAssetsPlugin(),
       new TerserPlugin(),
       new HtmlWebpackPlugin({
-        template: './src/template.html',
+        template: './src/index.html',
         favicon: './src/assets/favicon.png',
         minify: {
           removeAttributeQuotes: true,
